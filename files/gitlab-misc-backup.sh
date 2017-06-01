0c80d72fe1b5d391b924f1cb8fdf3b3dee11db7b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-06-01 14:22:50 +0900

    Wait for gitlab postgresql when startup

--- files/gitlab-misc-backup.sh
+++ files/gitlab-misc-backup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
 umask 077
+if [ ! -S /var/opt/gitlab/postgresql/.s.PGSQL.5432 ]; then
+  sleep 5
+fi
 /opt/gitlab/embedded/bin/chpst -u gitlab-psql /opt/gitlab/embedded/bin/pg_dump -p 5432 -h /var/opt/gitlab/postgresql mattermost_production > "/var/opt/gitlab/backups/$(date +%s_%Y_%m_%d)_mattermost_production_backup"
