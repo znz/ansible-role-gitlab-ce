99e034bd5dec5577189da9459ece237572ebbd7b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2018-03-20 18:22:20 +0900

    Improve backup
    - Add `-Fc` to `pg_dump`
    - Backup `/etc/gitlab` and `/var/opt/gitlab/mattermost` too

--- files/gitlab-misc-backup.sh
+++ files/gitlab-misc-backup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
+BACKUPS=/var/opt/gitlab/backups
 umask 077
 if [ ! -S /var/opt/gitlab/postgresql/.s.PGSQL.5432 ]; then
   sleep 5
 fi
-/opt/gitlab/embedded/bin/chpst -u gitlab-psql /opt/gitlab/embedded/bin/pg_dump -p 5432 -h /var/opt/gitlab/postgresql mattermost_production > "/var/opt/gitlab/backups/$(date +%s_%Y_%m_%d)_mattermost_production_backup"
+tar acf "$BACKUPS/$(date +%s_%Y_%m_%d)_etc_gitlab.tar.xz" -C / etc/gitlab
+tar acf "$BACKUPS/$(date +%s_%Y_%m_%d)_var_opt_gitlab_mattermost.tar.xz" -C / var/opt/gitlab/mattermost
+/opt/gitlab/embedded/bin/chpst -u gitlab-psql /opt/gitlab/embedded/bin/pg_dump -Fc -p 5432 -h /var/opt/gitlab/postgresql mattermost_production > "$BACKUPS/$(date +%s_%Y_%m_%d)_mattermost_production.dump"
